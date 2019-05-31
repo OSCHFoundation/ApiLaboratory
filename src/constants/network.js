3cf1b15d2e1b5ef02298d2846645f7ae145f19d9
Sawyer-li <13894705977@163.com>
2019-05-31 14:38:05 +0800

    修改路径

--- src/constants/network.js
+++ src/constants/network.js
@@ -3,16 +3,12 @@ import {Network, Networks} from 'stellar-sdk';
 const NETWORK = {
   available: {
     test: {
-      horizonURL: 'http://coast.oschain.io',  //https://horizon-testnet.stellar.org
-      //horizonURL: 'http://40.113.192.187:8000',  //https://horizon-testnet.stellar.org
-      //horizonURL: 'http://127.0.0.1:8000',  //https://horizon-testnet.stellar.org
-      networkPassphrase:  'osch public network'         //Networks.TESTNET
-//      horizonURL: 'https://horizon-testnet.stellar.org',
- //     networkPassphrase: Networks.TESTNET
+      horizonURL: 'http://tcoast.myoschain.com',  
+      networkPassphrase:  'osch test network'         //Networks.TESTNET
     },
     public: {
-      horizonURL: 'http://coast.oschain.io',
-      networkPassphrase: Networks.PUBLIC
+      horizonURL: 'http://coast.myoschain.com',
+      networkPassphrase: 'osch public network'
     }
   },
   defaultName: 'test',
